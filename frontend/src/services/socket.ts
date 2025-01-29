10d0c406460b3b543719c9bfc34021ac8552bfce
Abhishekkumar2021 <abhishekku20022002@gmail.com>
2025-01-29 06:13:00 +0530

    Updated frontend

--- frontend/src/services/socket.ts
+++ frontend/src/services/socket.ts
@@ -11,7 +11,7 @@ let socket: Socket | null = null;
 export const getSocket = (): Socket => {
   if (!socket) {
     
-    socket = io('http://localhost:8080');
+    socket = io(import.meta.env.VITE_SERVER_URL);
   }
   return socket;
 };
